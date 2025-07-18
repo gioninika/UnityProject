aa9daef7a93a7d2f90f2cbf40990f6b35cb231ed
gioninika <113819898+gioninika@users.noreply.github.com>
2025-07-18 21:27:10 +0400

    18

--- Assets/scripts/FinishScript.cs
+++ Assets/scripts/FinishScript.cs
@@ -1,14 +1,16 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class FinishScript : MonoBehaviour
 {
+    [SerializeField] private String Lvl;
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             UnlockNewLevel();
-            SceneManager.LoadScene("MainMenu");
+            SceneManager.LoadScene(Lvl);
         }
     }
     public void UnlockNewLevel()
